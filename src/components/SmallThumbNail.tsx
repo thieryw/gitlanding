d1227e7b4b525c018ba8d0b05bc167f2a4ca2fbf
William Thiery <williamalexthiery@hotmail.fr>
2021-06-10 14:22:13 +0200

    added background property to smallThumbNail

--- src/components/SmallThumbNail.tsx
+++ src/components/SmallThumbNail.tsx
@@ -1,8 +1,21 @@
+/* eslint-disable @typescript-eslint/no-namespace */
 import { memo } from "react";
 import { createUseClassNames } from "../theme";
 import Link from "@material-ui/core/Link";
 import { Typography } from "onyxia-ui/Typography";
 import { cx } from "tss-react";
+
+declare namespace BackgroundProps {
+    export type Image = {
+        type: "image";
+        url: string;
+    };
+
+    export type Color = {
+        type: "color";
+        color: string;
+    };
+}
 
 export type SmallThumbNailProps = {
     className?: string;
@@ -10,34 +23,46 @@ export type SmallThumbNailProps = {
     href?: string;
     width?: string | number;
     height?: string | number;
+    background?: BackgroundProps.Color | BackgroundProps.Image;
 };
 
-const { useClassNames } = createUseClassNames<Pick<SmallThumbNailProps, "width" | "height">>()(
-    (theme, { width, height }) => ({
-        "root": {
-            "height": height !== undefined ? height : 280,
-            "width": width !== undefined ? width : 557,
-            "borderRadius": 16,
-            "backgroundColor": theme.isDarkModeEnabled
-                ? theme.colors.palette.dark.greyVariant1
-                : theme.colors.palette.light.greyVariant1,
+const { useClassNames } = createUseClassNames<
+    Pick<SmallThumbNailProps, "width" | "height" | "background">
+>()((theme, { width, height, background }) => ({
+    "root": {
+        "height": height !== undefined ? height : 280,
+        "width": width !== undefined ? width : 557,
+        "borderRadius": 16,
 
-            "& a": {
-                "width": "100%",
-                "height": "100%",
-                "display": "flex",
-                "justifyContent": "center",
-                "alignItems": "center",
-            },
-            "cursor": "pointer",
+        "& a": {
+            "width": "100%",
+            "height": "100%",
+            "display": "flex",
+            "justifyContent": "center",
+            "alignItems": "center",
         },
-    }),
-);
+        "cursor": "pointer",
+        "background": (() => {
+            if (background === undefined) {
+                return theme.isDarkModeEnabled
+                    ? theme.colors.palette.dark.greyVariant1
+                    : theme.colors.palette.light.greyVariant1;
+            }
+
+            if (background.type === "color") {
+                return background.color;
+            }
+
+            return `url(${background.url}) no-repeat center`;
+        })(),
+        "backgroundSize": background?.type === "image" ? "cover" : undefined,
+    },
+}));
 
 export const SmallThumbNail = memo((props: SmallThumbNailProps) => {
-    const { className, height, href, title, width } = props;
+    const { className, height, href, title, width, background } = props;
 
-    const { classNames } = useClassNames({ width, height });
+    const { classNames } = useClassNames({ width, height, background });
 
     return (
         <div className={cx(classNames.root, className)}>
