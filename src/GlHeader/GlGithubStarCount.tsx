dbf42bd42fd4b3938d977f44dad8cb4980d391ce
thieryw <williamalexthiery@hotmail.fr>
2021-06-23 15:58:43 +0200

    Refactor star count

--- src/GlHeader/GlGithubStarCount.tsx
+++ src/GlHeader/GlGithubStarCount.tsx
@@ -1,4 +1,4 @@
-import { memo /*useEffect*/ } from "react";
+import { memo } from "react";
 import { useIsDarkModeEnabled } from "onyxia-ui/lib";
 import { cx } from "tss-react";
 import { getThemeApi } from "../theme";
@@ -32,15 +32,22 @@ export const GlGithubStarCount = memo((props: GlGithubStarCountProps) => {
     const { useClassNames } = useGuaranteedMemo(() => getUseClassNames(), []);
     const { classNames } = useClassNames({});
 
+    const { themeVariant } = (function useClosure() {
+        const { isDarkModeEnabled } = useIsDarkModeEnabled();
+
+        const themeVariant = isDarkModeEnabled ? "dark" : ("light" as const);
+
+        return { themeVariant };
+    })();
+
     return (
         <div className={cx(classNames.root, className)}>
             <GithubBtn
                 href={repoUrl}
-                data-color-scheme={`
-                    no-preference: light; 
-                    light: light; 
-                    dark: ${useIsDarkModeEnabled().isDarkModeEnabled ? "dark" : "light"};
-                `}
+                data-color-scheme={[
+                    "no-preference: light;",
+                    ...["light", "dark"].map(osPref => `${osPref}: ${themeVariant};`),
+                ].join("\n")}
                 data-icon="octicon-star"
                 data-size={size === "large" ? size : ""}
             >
