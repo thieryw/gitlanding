5792cecb54eedb16fa07c61e44f792dfcdf0eb66
thieryw <williamalexthiery@hotmail.fr>
2021-07-02 20:05:31 +0200

    added hacky css to correct the fact that  wrapLongLines props does not work

--- src/utils/GlCode.tsx
+++ src/utils/GlCode.tsx
@@ -1,5 +1,22 @@
 import { CodeBlock, dracula } from "react-code-blocks";
 import { memo } from "react";
+import { getThemeApi } from "../theme";
+import { useGuaranteedMemo } from "powerhooks";
+import { cx } from "tss-react";
+
+const getUseClassNames = () => {
+    const { createUseClassNames } = getThemeApi();
+
+    const { useClassNames } = createUseClassNames()(() => ({
+        "root": {
+            "& code": {
+                "width": 0,
+            },
+        },
+    }));
+
+    return { useClassNames };
+};
 
 export type GlCodeProps = {
     text?: string;
@@ -11,8 +28,12 @@ export type GlCodeProps = {
 export const GlCode = memo((props: GlCodeProps) => {
     const { className, language, showLineNumbers, text } = props;
 
+    const { useClassNames } = useGuaranteedMemo(() => getUseClassNames(), []);
+
+    const { classNames } = useClassNames({});
+
     return (
-        <div className={className}>
+        <div className={cx(classNames.root, className)}>
             <CodeBlock
                 language={language}
                 showLineNumbers={showLineNumbers}
