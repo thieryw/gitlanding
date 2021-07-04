e2d29019e305e130bc83becbe754cb4c79460895
thieryw <williamalexthiery@hotmail.fr>
2021-07-04 21:22:10 +0200

    added vs code buttons for GlCode component

--- src/utils/GlCode.tsx
+++ src/utils/GlCode.tsx
@@ -1,5 +1,35 @@
 import { CodeBlock, dracula } from "react-code-blocks";
 import { memo } from "react";
+import { getThemeApi } from "../theme";
+import { cx, css } from "tss-react";
+import { useGuaranteedMemo } from "powerhooks";
+
+const colors = {
+    "tomatoRed": "#f85b52",
+    "goldenRoad": "#e1bb2a",
+    "limeGreen": "#54bd2b",
+    "darkslategray": "#282a36",
+};
+
+const getUseClassNames = () => {
+    const { createUseClassNames } = getThemeApi();
+
+    const { useClassNames } = createUseClassNames()(theme => ({
+        "root": {
+            "position": "relative",
+            "borderTop": `solid ${colors.darkslategray} 24px`,
+        },
+        "vsCodeButtons": {
+            "position": "absolute",
+            "top": theme.spacing(-2),
+            "left": theme.spacing(1),
+        },
+    }));
+
+    return {
+        useClassNames,
+    };
+};
 
 export type GlCodeProps = {
     text?: string;
@@ -11,8 +41,13 @@ export type GlCodeProps = {
 export const GlCode = memo((props: GlCodeProps) => {
     const { className, language, showLineNumbers, text } = props;
 
+    const { useClassNames } = useGuaranteedMemo(() => getUseClassNames(), []);
+
+    const { classNames } = useClassNames({});
+
     return (
-        <div className={className}>
+        <div className={cx(classNames.root, className)}>
+            <VsCodeButtons className={classNames.vsCodeButtons} />
             <CodeBlock
                 language={language}
                 showLineNumbers={showLineNumbers}
@@ -22,3 +57,55 @@ export const GlCode = memo((props: GlCodeProps) => {
         </div>
     );
 });
+
+const { VsCodeButtons } = (() => {
+    const { createUseClassNames } = getThemeApi();
+
+    const { useClassNames } = createUseClassNames()(theme => ({
+        "root": {
+            "display": "flex",
+            "gap": theme.spacing(1),
+        },
+        "icon": {
+            ...(() => {
+                const value = 14;
+                return {
+                    "width": value,
+                    "height": value,
+                };
+            })(),
+            "borderRadius": "50%",
+        },
+    }));
+
+    type VsCodeButtonsProps = {
+        className?: string;
+    };
+
+    const VsCodeButtons = memo((props: VsCodeButtonsProps) => {
+        const { className } = props;
+
+        const { classNames } = useClassNames({});
+        return (
+            <div className={cx(classNames.root, className)}>
+                {[colors.tomatoRed, colors.goldenRoad, colors.limeGreen].map(
+                    color => {
+                        return (
+                            <div
+                                className={cx(
+                                    classNames.icon,
+                                    css({
+                                        "backgroundColor": color,
+                                    }),
+                                )}
+                                key={color}
+                            ></div>
+                        );
+                    },
+                )}
+            </div>
+        );
+    });
+
+    return { VsCodeButtons };
+})();
