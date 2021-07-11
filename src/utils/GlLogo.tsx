df8358ca9951d337311e5b04594ea3c22d5f9a30
thieryw <williamalexthiery@hotmail.fr>
2021-07-12 00:06:06 +0200

    GlLogo if no fill on svg or in props then automaticaly set to theme values

--- src/utils/GlLogo.tsx
+++ src/utils/GlLogo.tsx
@@ -1,16 +1,38 @@
 import { ReactSVG } from "react-svg";
 import { memo } from "react";
+import { getThemeApi } from "../theme";
+import { useGuaranteedMemo } from "powerhooks";
+import { cx } from "tss-react";
+
+const getUseClassNames = () => {
+    const { createUseClassNames } = getThemeApi();
+
+    const { useClassNames } = createUseClassNames<{
+        fill: string | undefined;
+    }>()((theme, { fill }) => ({
+        "svg": {
+            "fill": fill ?? theme.colors.useCases.typography.textPrimary,
+        },
+    }));
+
+    return { useClassNames };
+};
 
 type GlLogoProps = {
     logoUrl: string;
     className?: string;
+    fill?: string;
 };
 
 export const GlLogo = memo((props: GlLogoProps) => {
-    const { className, logoUrl } = props;
+    const { className, logoUrl, fill } = props;
+
+    const { useClassNames } = useGuaranteedMemo(() => getUseClassNames(), []);
+
+    const { classNames } = useClassNames({ fill });
 
     return logoUrl.endsWith(".svg") ? (
-        <ReactSVG src={logoUrl} className={className} />
+        <ReactSVG src={logoUrl} className={cx(classNames.svg, className)} />
     ) : (
         <img src={logoUrl} className={className} alt="logo" />
     );
