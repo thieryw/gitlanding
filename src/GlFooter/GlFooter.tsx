907434cddafc6271c31b390c72df405248ba4e72
thieryw <williamalexthiery@hotmail.fr>
2021-07-13 13:33:46 +0200

    update api, added marginTop, background Color, added transition on icon hover and added flex wrap for iconslinks and links

--- src/GlFooter/GlFooter.tsx
+++ src/GlFooter/GlFooter.tsx
@@ -13,10 +13,7 @@ type Link = {
 
 export type GlFooterProps = {
     className?: string;
-    iconLinks?: {
-        iconUrl: string;
-        link: Link;
-    }[];
+    iconLinks?: (Link & { iconUrl: string })[];
     links?: (Link & { title: string })[];
     info?: ReactNode;
     bottomDiv?: ReactNode;
@@ -31,6 +28,8 @@ const getUseClassNames = () => {
             "flexDirection": "column",
             "alignItems": "center",
             "justifyContent": "center",
+            "marginTop": theme.spacing(8),
+            "backgroundColor": theme.colors.useCases.surfaces.surface2,
             ...(() => {
                 const value = theme.spacing(4);
 
@@ -42,8 +41,13 @@ const getUseClassNames = () => {
             })(),
         },
         "icon": {
+            "transition": "transform 200ms",
+            ":hover": {
+                "cursor": "pointer",
+                "transform": "scale(1.2)",
+            },
             ...(() => {
-                const value = theme.spacing(6);
+                const value = theme.spacing(5);
 
                 return {
                     "width": value,
@@ -58,8 +62,11 @@ const getUseClassNames = () => {
         "icons": {
             "display": "flex",
             "flexDirection": "row",
+            "flexWrap": "wrap",
+            "justifyContent": "center",
             "gap": theme.spacing(4),
             "marginBottom": theme.spacing(3),
+            "marginTop": theme.spacing(2),
         },
         "link": {
             "color": theme.colors.useCases.typography.textPrimary,
@@ -67,9 +74,11 @@ const getUseClassNames = () => {
         "links": {
             "display": "flex",
             "flexDirection": "row",
+            "flexWrap": "wrap",
+            "justifyContent": "center",
             "gap": theme.spacing(4),
             "marginTop": theme.spacing(3),
-            "marginBottom": theme.spacing(6),
+            "marginBottom": theme.spacing(3),
         },
     }));
 
@@ -87,13 +96,14 @@ export const GlFooter = memo((props: GlFooterProps) => {
         <footer className={cx(classNames.root, className)}>
             {iconLinks !== undefined && (
                 <div className={classNames.icons}>
-                    {iconLinks.map(iconLink => (
+                    {iconLinks.map((iconLink, index) => (
                         <div
+                            key={index}
                             onClick={
-                                iconLink.link.onClick ??
+                                iconLink.onClick ??
                                 (() =>
                                     (window.location.href =
-                                        iconLink.link.href ?? "#"))
+                                        iconLink.href ?? "#"))
                             }
                         >
                             <GlLogo
@@ -107,8 +117,9 @@ export const GlFooter = memo((props: GlFooterProps) => {
 
             {links !== undefined && (
                 <div className={classNames.links}>
-                    {links.map(link => (
+                    {links.map((link, index) => (
                         <Link
+                            key={index}
                             href={link.href}
                             onClick={link.onClick}
                             className={classNames.link}
