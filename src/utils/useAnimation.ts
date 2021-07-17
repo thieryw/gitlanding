133d5523bfb59902566c1303cd2813d6687192c3
thieryw <williamalexthiery@hotmail.fr>
2021-07-18 00:37:09 +0200

    A seemingly functioning version of animation function

--- src/utils/useAnimation.ts
+++ src/utils/useAnimation.ts
@@ -1,47 +1,87 @@
 import { useRef } from "react";
 
 export function useAnimation<T extends HTMLElement = any>(params: {
-    animationType: "fade";
     animationDuration?: number;
     animationDelay?: number;
+    direction?:
+        | "left"
+        | "right"
+        | "top"
+        | "bottom"
+        | "topLeft"
+        | "topRight"
+        | "bottomLeft"
+        | "bottomRight";
+    rootMargin?: string;
 }) {
     const rootRef = useRef<T>(null);
-    const { animationType, animationDelay, animationDuration } = params;
+    const { direction, animationDelay, animationDuration, rootMargin } = params;
 
-    function fade() {
+    function animate() {
         if (!rootRef.current) {
             return;
         }
 
-        const style = rootRef.current.style;
+        const ref = rootRef.current;
+        const style = ref.style;
 
         style.opacity = "0%";
-        style.transition = `opacity ${animationDuration ?? 300}ms`;
-        if (animationDelay !== undefined) {
-            style.transitionDelay = `${animationDelay}ms`;
-        }
 
-        const observer = new IntersectionObserver(entries => {
-            if (!rootRef.current) {
-                return;
+        style.transform = (() => {
+            switch (direction) {
+                case "left":
+                    return "translateX(-150px)";
+                case "top":
+                    return "translateY(-150px)";
+                case "right":
+                    return "translateX(150px)";
+                case "bottom":
+                    return "translateY(150px)";
+                case "topLeft":
+                    return "translate(-150px, -150px)";
+                case "topRight":
+                    return "translate(150px, -150px)";
+                case "bottomLeft":
+                    return "translate(-150px, 150px)";
+                case "bottomRight":
+                    return "translate(150px, 150px)";
+                case undefined:
+                    return "none";
             }
-            if (entries[0].isIntersecting) {
-                style.opacity = "100%";
-                return;
-            }
+        })();
 
-            style.opacity = "0%";
-        });
+        const observe = () => {
+            const observer = new IntersectionObserver(
+                entries => {
+                    if (entries[0].isIntersecting) {
+                        style.transition = (() => {
+                            if (direction === undefined) {
+                                return `opacity ${animationDuration ?? 300}ms`;
+                            }
 
-        observer.observe(rootRef.current);
+                            return [
+                                "opacity 300ms,",
+                                `transform ${animationDuration ?? 300}ms`,
+                            ].join(" ");
+                        })();
+
+                        style.transitionDelay = `${animationDelay ?? 0}ms`;
+                        style.opacity = "100%";
+                        if (direction !== undefined) {
+                            style.transform = "none";
+                        }
+                        style.transitionTimingFunction = "ease-in-out";
+                        observer.unobserve(entries[0].target);
+                        window.removeEventListener("load", observe);
+                        return;
+                    }
+                },
+                { rootMargin },
+            );
+            observer.observe(ref);
+        };
+        window.addEventListener("load", observe);
     }
-
-    const animate = (() => {
-        switch (animationType) {
-            case "fade":
-                return fade;
-        }
-    })();
 
     return { rootRef, animate };
 }
