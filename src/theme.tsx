a9f0140a39c8348e256016b702de16b11cd53bff
garronej <joseph.garrone.gj@gmail.com>
2021-07-20 00:26:33 +0200

    update deps

--- src/theme.tsx
+++ src/theme.tsx
@@ -6,7 +6,6 @@ import { createIcon } from "onyxia-ui/Icon";
 import { createIconButton } from "onyxia-ui/IconButton";
 import { createButton } from "onyxia-ui/Button";
 
-import IconButtonIcon from "@material-ui/core/IconButton";
 import Brightness4Icon from "@material-ui/icons/Brightness4";
 import Brightness7Icon from "@material-ui/icons/Brightness7";
 import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
@@ -29,7 +28,6 @@ export const { ThemeProvider: ThemeProviderDefault } = createThemeProvider({
 export const { makeStyles } = createMakeStyles({ useTheme });
 
 export const { Icon } = createIcon({
-    "iconButton": IconButtonIcon,
     "brightness4": Brightness4Icon,
     "brightness7": Brightness7Icon,
     "arrowBackIos": ArrowBackIosIcon,
