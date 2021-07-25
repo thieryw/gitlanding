ac4f6dd0fb8d3ed07b286580f979f3988ee0db14
thieryw <williamalexthiery@hotmail.fr>
2021-07-25 13:11:17 +0200

    rotating dark mode switch

--- src/GlHeader/GlDarkModeSwitch.tsx
+++ src/GlHeader/GlDarkModeSwitch.tsx
@@ -1,7 +1,9 @@
 import { memo } from "react";
 import { useConstCallback } from "powerhooks/useConstCallback";
+import { useNamedState } from "powerhooks";
 import { useIsDarkModeEnabled } from "onyxia-ui/lib";
 import { GlIconButton } from "../utils/GlIconButton";
+import { motion } from "framer-motion";
 
 export type GlDarkModeSwitchProps = {
     className?: string;
@@ -10,16 +12,24 @@ export type GlDarkModeSwitchProps = {
 export const GlDarkModeSwitch = memo((props: GlDarkModeSwitchProps) => {
     const { className } = props;
     const { isDarkModeEnabled, setIsDarkModeEnabled } = useIsDarkModeEnabled();
+    const { motionProps, setMotionProps } = useNamedState("motionProps", {
+        "rotate": 0,
+    });
 
-    const onClick = useConstCallback(() =>
-        setIsDarkModeEnabled(!isDarkModeEnabled),
-    );
+    const onClick = useConstCallback(() => {
+        setIsDarkModeEnabled(!isDarkModeEnabled);
+        setMotionProps({
+            "rotate": motionProps.rotate === 0 ? 360 : 0,
+        });
+    });
 
     return (
-        <GlIconButton
-            className={className}
-            iconId={isDarkModeEnabled ? "brightness7" : "brightness4"}
-            onClick={onClick}
-        />
+        <motion.div animate={motionProps}>
+            <GlIconButton
+                className={className}
+                iconId={isDarkModeEnabled ? "brightness7" : "brightness4"}
+                onClick={onClick}
+            />
+        </motion.div>
     );
 });
