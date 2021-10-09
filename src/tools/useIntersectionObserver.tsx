c293b3eb13cd06d3ca77996335c2c286726a049b
thieryw <williamalexthiery@hotmail.fr>
2021-10-09 23:25:48 +0200

    useIntersectionObserver hook, handy for triggering animations

---
+++ src/tools/useIntersectionObserver.tsx
@@ -0,0 +1,39 @@
+import { useEffect, useRef, useMemo } from "react";
+import type { RefObject } from "react";
+
+export function useIntersectionObserver<T extends HTMLElement = any>(params: {
+    callback: (params: {
+        entry: IntersectionObserverEntry;
+        observer: IntersectionObserver;
+    }) => void;
+    rootMargin?: string;
+    root?: Element | Document;
+    threshold?: number | number[];
+}): {
+    ref: RefObject<T>;
+} {
+    const { callback, rootMargin, root, threshold } = params;
+
+    const ref = useRef<T>(null);
+
+    const observer = useMemo(() => {
+        return new IntersectionObserver(
+            entries => {
+                callback({
+                    "entry": entries[0],
+                    observer,
+                });
+            },
+            { rootMargin, root, threshold },
+        );
+    }, []);
+
+    useEffect(() => {
+        if (!ref.current) {
+            return;
+        }
+        observer.observe(ref.current);
+    }, []);
+
+    return { ref };
+}
