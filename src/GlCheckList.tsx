169500510c5c4c905025f3b39b63b97e2b502a6b
thieryw <williamalexthiery@hotmail.fr>
2021-11-22 23:08:35 +0100

    animation for GlCheckList

--- src/GlCheckList.tsx
+++ src/GlCheckList.tsx
@@ -1,11 +1,14 @@
-import { memo } from "react";
+import { memo, useMemo, useReducer } from "react";
 import CheckIcon from "@mui/icons-material/Check";
 import { Text } from "./theme";
 import { Markdown } from "./tools/Markdown";
 import { makeStyles, breakpointsValues } from "./theme";
+import { useIntersectionObserver } from "./tools/useIntersectionObserver";
+import { motion } from "framer-motion";
 
 export type GlCheckListProps = {
     className?: string;
+    hasAnimation?: boolean;
     classes?: {
         headingWrapper?: string;
         heading?: string;
@@ -32,8 +35,70 @@ export const GlCheckList = memo((props: GlCheckListProps) => {
         elements,
         heading,
         subHeading,
+        hasAnimation,
         classes: classesProp,
     } = props;
+
+    const [, forceUpdate] = useReducer(x => x + 1, 0);
+
+    const container = useMemo(() => {
+        if (hasAnimation === undefined || !hasAnimation) {
+            return undefined;
+        }
+        return {
+            "show": {},
+            "hidden": { "opacity": 0 },
+        };
+    }, []);
+
+    const listItem = useMemo(() => {
+        if (hasAnimation === undefined || !hasAnimation) {
+            return undefined;
+        }
+
+        return {
+            "hidden": {
+                "opacity": 0,
+                "y": -40,
+            },
+            "show": {},
+        };
+    }, []);
+
+    const { ref } = useIntersectionObserver({
+        "callback": ({ observer, entry }) => {
+            if (hasAnimation === undefined || !hasAnimation) {
+                observer.unobserve(entry.target);
+                return;
+            }
+
+            if (container === undefined || listItem === undefined) {
+                return;
+            }
+
+            if (entry.isIntersecting) {
+                container.show = {
+                    "transition": {
+                        "staggerChildren": 0.2,
+                    },
+                    "opacity": 1,
+                };
+
+                listItem.show = {
+                    "opacity": 1,
+                    "y": 0,
+                    "transition": {
+                        "duration": 1,
+                        "ease": "easeOut",
+                    },
+                };
+
+                observer.unobserve(entry.target);
+                forceUpdate();
+            }
+        },
+        "threshold": 0.6,
+    });
 
     const { classes, cx } = useStyles({
         "numberOfElements": elements === undefined ? 1 : elements.length,
@@ -72,25 +137,34 @@ export const GlCheckList = memo((props: GlCheckListProps) => {
                 </div>
             )}
 
-            <div className={cx(classes.elements, classesProp?.elements)}>
+            <motion.div
+                ref={ref}
+                variants={container}
+                initial="hidden"
+                animate="show"
+                className={cx(classes.elements, classesProp?.elements)}
+            >
                 {elements !== undefined &&
                     elements.map(elementProps => (
-                        <CheckListElement
-                            key={elementProps.description}
-                            className={classesProp?.element}
-                            classes={{
-                                "checkIcon": classesProp?.checkIcon,
-                                "checkIconWrapper":
-                                    classesProp?.checkIconWrapper,
-                                "description": classesProp?.elementDescription,
-                                "title": classesProp?.elementTitle,
-                                "titleAndDescriptionWrapper":
-                                    classesProp?.elementTitleAndDescriptionWrapper,
-                            }}
-                            {...elementProps}
-                        />
+                        <motion.div variants={listItem}>
+                            <CheckListElement
+                                key={elementProps.description}
+                                className={classesProp?.element}
+                                classes={{
+                                    "checkIcon": classesProp?.checkIcon,
+                                    "checkIconWrapper":
+                                        classesProp?.checkIconWrapper,
+                                    "description":
+                                        classesProp?.elementDescription,
+                                    "title": classesProp?.elementTitle,
+                                    "titleAndDescriptionWrapper":
+                                        classesProp?.elementTitleAndDescriptionWrapper,
+                                }}
+                                {...elementProps}
+                            />
+                        </motion.div>
                     ))}
-            </div>
+            </motion.div>
         </section>
     );
 });
