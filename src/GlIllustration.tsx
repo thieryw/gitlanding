52645ff36180dd0c4cfb7c4eb2aa03a195752e85
thieryw <williamalexthiery@hotmail.fr>
2022-04-13 21:43:18 +0200

    GlIllustration add video type

--- src/GlIllustration.tsx
+++ src/GlIllustration.tsx
@@ -4,13 +4,19 @@ import { GlCodeBlock } from "./GlCodeBlock";
 import type { GlCodeBlockProps } from "./GlCodeBlock";
 import { memo } from "react";
 import { makeStyles } from "./theme";
+import type { GlVideoProps } from "./utils/GlVideo";
+import { GlVideo } from "./utils/GlVideo";
 
 export declare namespace GlIllustrationProps {
-    export type Illustration = Illustration.Code | Illustration.Image;
+    export type Illustration =
+        | Illustration.Code
+        | Illustration.Image
+        | Illustration.Video;
 
     export namespace Illustration {
         type Code = { type: "code" } & GlCodeBlockProps;
         type Image = { type: "image" } & GlImageProps;
+        type Video = { type: "video" } & GlVideoProps;
     }
 }
 
@@ -25,7 +31,21 @@ export const GlIllustration = memo((props: GlIllustrationProps) => {
 
     return (
         <div className={cx(classes.root, className)}>
-            {props.type === "image" ? (
+            {(() => {
+                const commonProps = {
+                    hasShadow,
+                    "className": classes.content,
+                };
+                switch (props.type) {
+                    case "code":
+                        return <GlCodeBlock {...commonProps} {...props} />;
+                    case "image":
+                        return <GlImage {...commonProps} {...props} />;
+                    case "video":
+                        return <GlVideo {...commonProps} {...props} />;
+                }
+            })()}
+            {/*props.type === "image" ? (
                 <GlImage
                     url={props.url}
                     alt={props.alt}
@@ -48,7 +68,7 @@ export const GlIllustration = memo((props: GlIllustrationProps) => {
                     isCopyBlock={props.isCopyBlock}
                     copiedToClipboardMessage={props.copiedToClipboardMessage}
                 />
-            )}
+                )*/}
         </div>
     );
 });
