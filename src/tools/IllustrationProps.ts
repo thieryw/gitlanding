945268e32d09fd548cab59340b22c7c999568017
thieryw <williamalexthiery@hotmail.fr>
2022-05-19 12:15:10 +0200

    create IllustrationProps

---
+++ src/tools/IllustrationProps.ts
@@ -0,0 +1,35 @@
+import type { Source } from "./Source";
+import type { ReactComponent } from "./ReactComponent";
+
+export type IllustrationProps =
+    | IllustrationProps.Image
+    | IllustrationProps.Video
+    | IllustrationProps.CustomComponent;
+
+declare namespace IllustrationProps {
+    export type Image = {
+        type: "image";
+        sources?: Source[];
+        src: string;
+        hasShadow?: boolean;
+    };
+
+    export type Video = {
+        type: "video";
+        sources: Source[];
+        hasShadow?: boolean;
+        autoPlay?: boolean;
+        muted?: boolean;
+        loop?: boolean;
+        controls?: boolean;
+    };
+
+    export type CustomComponent = {
+        type: "custom component";
+        Component: ReactComponent<{
+            className: string;
+            onLoad: () => void;
+            id: string;
+        }>;
+    };
+}
