aadaca1479fca9b92bea5385b4fed23a66343fe1
thieryw <williamalexthiery@hotmail.fr>
2022-06-26 14:38:01 +0200

    remove Component type

--- src/tools/ComponentType.tsx
+++
@@ -1,7 +0,0 @@
-/* eslint-disable @typescript-eslint/ban-types */
-
-import type { FC, ComponentClass } from "react";
-
-export type ComponentType<Props extends Record<string, unknown> = {}> =
-    | ((props: Props) => ReturnType<FC>)
-    | ComponentClass<Props>;
