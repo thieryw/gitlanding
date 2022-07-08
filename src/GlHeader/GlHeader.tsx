1de0e2bf1558c52bb440f76abcb4bbb806fc50fb
thieryw <williamalexthiery@hotmail.fr>
2022-07-08 19:43:34 +0200

    GlHeader accomodate custom items for small screens

--- src/GlHeader/GlHeader.tsx
+++ src/GlHeader/GlHeader.tsx
@@ -1,4 +1,4 @@
-import { memo, useState, useEffect } from "react";
+import { memo, useState, useEffect, useCallback } from "react";
 import type { ReactNode } from "react";
 import { makeStyles, breakpointsValues, Text } from "../theme";
 import { GlHeaderLinks } from "./GlHeaderLinks";
@@ -11,6 +11,13 @@ import { getScrollableParent } from "powerhooks/getScrollableParent";
 import { useDomRect } from "powerhooks/useDomRect";
 import { useEvt } from "evt/hooks/useEvt";
 import { Evt } from "evt";
+
+type Behavior = "hide" | "wrap" | "normal";
+
+type CustomItem = {
+    item: NonNullable<ReactNode>;
+    behaviorOnSmallDevice?: Behavior;
+}
 
 export type GlHeaderProps = {
     links: {
@@ -29,9 +36,12 @@ export type GlHeaderProps = {
     githubRepoUrl?: string;
     githubButtonSize?: "normal" | "large";
     showGithubStarCount?: boolean;
-    customItemStart?: NonNullable<ReactNode>;
-    customItemEnd?: NonNullable<ReactNode>;
+    customItemStart?: CustomItem;
+    customItemEnd?: CustomItem;
 };
+
+
+
 export const GlHeader = memo((props: GlHeaderProps) => {
     const {
         links,
@@ -131,7 +141,21 @@ export const GlHeader = memo((props: GlHeaderProps) => {
         [ref.current, headerHeight],
     );
 
-    const { theme, classes, cx } = useStyles({ isSmallDevice }, { props });
+    const getCustomItemBehavior = useCallback((customItem: CustomItem | undefined): Behavior=>{
+        if(customItem === undefined){
+            return "normal";
+        }
+        return customItem.behaviorOnSmallDevice ?? "normal";
+    }, [
+        customItemStart?.behaviorOnSmallDevice,
+        customItemEnd?.behaviorOnSmallDevice
+    ])
+
+    const { theme, classes, cx } = useStyles({ 
+        isSmallDevice ,
+        "customItemStartSmallBehavior": getCustomItemBehavior(customItemStart),
+        "customItemEndSmallBehavior": getCustomItemBehavior(customItemEnd)
+    }, { props });
 
     useEffect(() => {
         if (customBreakpoint !== undefined) {
@@ -149,97 +173,153 @@ export const GlHeader = memo((props: GlHeaderProps) => {
 
     return (
         <header ref={ref} className={cx(classes.root, className)}>
-            {(() => {
-                return (
-                    <div ref={titleRef} className={classes.titleWrapper}>
-                        {(() => {
-                            const transformElementIfString = (
-                                element: ReactNode,
-                            ) => {
-                                if (typeof element === "string") {
-                                    return (
-                                        <Text
-                                            className={classes.titleText}
-                                            typo="subtitle"
-                                        >
-                                            {element}
-                                        </Text>
+            <div className={classes.largeScreenContentWrapper}>
+                {(() => {
+                    return (
+                        <div ref={titleRef} className={classes.titleWrapper}>
+                            {(() => {
+                                const transformElementIfString = (
+                                    element: ReactNode,
+                                ) => {
+                                    if (typeof element === "string") {
+                                        return (
+                                            <Text
+                                                className={classes.titleText}
+                                                typo="subtitle"
+                                            >
+                                                {element}
+                                            </Text>
+                                        );
+                                    }
+                                    return element;
+                                };
+
+                                if (
+                                    theme.windowInnerWidth >=
+                                    breakpointsValues.md
+                                ) {
+                                    if (!theme.isDarkModeEnabled) {
+                                        return transformElementIfString(title);
+                                    }
+                                    return transformElementIfString(
+                                        titleDark ?? title,
                                     );
                                 }
-                                return element;
-                            };
 
-                            if (
-                                theme.windowInnerWidth >= breakpointsValues.md
-                            ) {
                                 if (!theme.isDarkModeEnabled) {
-                                    return transformElementIfString(title);
+                                    return transformElementIfString(
+                                        titleSmallScreen ?? title,
+                                    );
                                 }
+
                                 return transformElementIfString(
-                                    titleDark ?? title,
+                                    titleSmallScreenDark ??
+                                        titleSmallScreen ??
+                                        titleDark ??
+                                        title,
                                 );
-                            }
-
-                            if (!theme.isDarkModeEnabled) {
-                                return transformElementIfString(
-                                    titleSmallScreen ?? title,
-                                );
-                            }
-
-                            return transformElementIfString(
-                                titleSmallScreenDark ??
-                                    titleSmallScreen ??
-                                    titleDark ??
-                                    title,
-                            );
-                        })()}
-                    </div>
-                );
-            })()}
-
-            <div
-                ref={buttonsAndLinksRef}
-                className={classes.linkAndButtonWrapper}
-            >
-                {customItemStart !== undefined && customItemStart}
-                <div className={classes.linksWrapperLargeScreen}>
-                    <GlHeaderLinks
-                        classes={{
-                            "contentWrapper": classes.linksContentWrapper,
-                        }}
-                        className={classes.links}
-                        links={links.map(link => ({
-                            ...link,
-                            "classes": {
-                                "link": classes.link,
-                                "underline": classes.underline,
-                            },
-                            "className": classes.linkRoot,
-                        }))}
-                        type="largeScreen"
-                    />
-                </div>
-                {githubRepoUrl !== undefined && (
-                    <GlGithubStarCount
-                        repoUrl={githubRepoUrl}
-                        size={githubButtonSize}
-                        showCount={showGithubStarCount}
-                        className={classes.githubStar}
-                    />
-                )}
-                {enableDarkModeSwitch !== undefined && enableDarkModeSwitch && (
-                    <GlDarkModeSwitch className={classes.darkModeSwitch} />
-                )}
-
-                {customItemEnd !== undefined && customItemEnd}
+                            })()}
+                        </div>
+                    );
+                })()}
 
                 <div
-                    className={classes.unfoldIconWrapper}
-                    onClick={toggleMenuUnfolded}
+                    ref={buttonsAndLinksRef}
+                    className={classes.linkAndButtonWrapper}
                 >
-                    <UnfoldIcon className={classes.unfoldIcon} />
+                    {
+                        customItemStart !== undefined &&
+                        <div
+                            className={cx(
+                                classes.commonCustomItemWrapper,
+                                classes.customItemStartWrapper,
+                            )}
+                        >
+                            {customItemStart.item}
+                        </div>
+                    }
+                    <div className={classes.linksWrapperLargeScreen}>
+                        <GlHeaderLinks
+                            classes={{
+                                "contentWrapper": classes.linksContentWrapper,
+                            }}
+                            className={classes.links}
+                            links={links.map(link => ({
+                                ...link,
+                                "classes": {
+                                    "link": classes.link,
+                                    "underline": classes.underline,
+                                },
+                                "className": classes.linkRoot,
+                            }))}
+                            type="largeScreen"
+                        />
+                    </div>
+                    {githubRepoUrl !== undefined && (
+                        <GlGithubStarCount
+                            repoUrl={githubRepoUrl}
+                            size={githubButtonSize}
+                            showCount={showGithubStarCount}
+                            className={classes.githubStar}
+                        />
+                    )}
+                    {enableDarkModeSwitch !== undefined &&
+                        enableDarkModeSwitch && (
+                            <GlDarkModeSwitch
+                                className={classes.darkModeSwitch}
+                            />
+                        )}
+
+                    {
+                        customItemEnd !== undefined &&
+                        <div
+                            className={cx(
+                                classes.commonCustomItemWrapper,
+                                classes.customItemEndWrapper,
+                            )}
+                        >
+                            {customItemEnd.item}
+                        </div>
+
+                    }
+
+                    <div
+                        className={classes.unfoldIconWrapper}
+                        onClick={toggleMenuUnfolded}
+                    >
+                        <UnfoldIcon className={classes.unfoldIcon} />
+                    </div>
                 </div>
             </div>
+            {
+                (customItemStart !== undefined || customItemEnd !== undefined) &&
+                <div className={classes.smallDeviceCustomItemsWrapper}>
+                    {
+                        customItemStart !== undefined &&
+                        <div
+                            className={cx(
+                                classes.commonSmallDeviceCustomItemWrapper,
+                                classes.smallDeviceCustomItemStartWrapper,
+                            )}
+                        >
+                            {customItemStart.item}
+                        </div>
+                    }
+
+                    {
+                        customItemEnd !== undefined &&
+                        <div
+                            className={cx(
+                                classes.commonSmallDeviceCustomItemWrapper,
+                                classes.smallDeviceCustomItemEndWrapper,
+                            )}
+                        >
+                            {customItemEnd.item}
+                        </div>
+                    }
+
+                </div>
+            }
             <GlHeaderLinks
                 classes={{
                     "contentWrapper": classes.linksContentWrapperSmallScreen,
@@ -261,9 +341,30 @@ export const GlHeader = memo((props: GlHeaderProps) => {
     );
 });
 
-const useStyles = makeStyles<{ isSmallDevice: boolean | undefined }>({
+const useStyles = makeStyles<
+    {
+        isSmallDevice: boolean | undefined;
+        customItemStartSmallBehavior: Behavior;
+        customItemEndSmallBehavior: Behavior;
+    }
+>({
     "name": { GlHeader },
-})((theme, { isSmallDevice }) => {
+})((theme, { isSmallDevice, customItemEndSmallBehavior, customItemStartSmallBehavior }) => {
+
+    function getSmallDeviceCustomItemDisplay(behavior: Behavior) {
+        if (isSmallDevice && behavior === "wrap") {
+            return undefined;
+        }
+        return "none";
+    }
+
+    function getCustomItemDisplay(behavior: Behavior) {
+        if (!isSmallDevice) {
+            return undefined;
+        }
+        return behavior === "normal" ? undefined : "none"
+    }
+
     return {
         "root": {
             "padding": theme.spacing({
@@ -271,12 +372,14 @@ const useStyles = makeStyles<{ isSmallDevice: boolean | undefined }>({
                 "topBottom": `${theme.spacing(3)}px`,
             }),
             "position": "relative",
-            "display": "flex",
-            "justifyContent": "space-between",
-            "alignItems": "center",
             "opacity": isSmallDevice === undefined ? 0 : 1,
             "maxWidth": "100%",
             "overflowX": !isSmallDevice ? "hidden" : undefined,
+        },
+        "largeScreenContentWrapper": {
+            "display": "flex",
+            "justifyContent": "space-between",
+            "alignItems": "center",
         },
         "titleWrapper": {
             "marginRight": isSmallDevice ? undefined : theme.spacing(8),
@@ -311,6 +414,35 @@ const useStyles = makeStyles<{ isSmallDevice: boolean | undefined }>({
         "linksWrapperLargeScreen": {
             "order": isSmallDevice ? -1 : undefined,
         },
+        "smallDeviceCustomItemsWrapper": {
+            "display": !isSmallDevice ? "none" : "grid",
+            "gridAutoFlow": "row",
+            "alignItems": "end",
+            ...(() => {
+                const value = theme.spacing(3);
+                return {
+                    "gap": value,
+                    ...theme.spacing.topBottom("margin", `${value}px`),
+                };
+            })(),
+        },
+        "commonSmallDeviceCustomItemWrapper": {
+            "display": "flex",
+            "justifyContent": "flex-end",
+        },
+        "smallDeviceCustomItemStartWrapper": {
+            "display": getSmallDeviceCustomItemDisplay(customItemStartSmallBehavior)
+        },
+        "smallDeviceCustomItemEndWrapper": {
+            "display": getSmallDeviceCustomItemDisplay(customItemEndSmallBehavior)
+        },
+        "customItemStartWrapper": {
+            "display": getCustomItemDisplay(customItemStartSmallBehavior)
+        },
+        "customItemEndWrapper": {
+            "display": getCustomItemDisplay(customItemEndSmallBehavior)
+        },
+        "commonCustomItemWrapper": {},
         "unfoldIcon": {},
         "githubStar": {},
         "darkModeSwitch": {},
