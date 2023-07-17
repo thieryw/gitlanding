dfcebc75afdf6098c39aecd0f8791387dd9bcfc9
garronej <joseph.garrone@data.gouv.fr>
2023-07-17 05:59:34 +0200

    Fix previous commit

--- src/utils/GlVideo.tsx
+++ src/utils/GlVideo.tsx
@@ -2,6 +2,8 @@ import { memo, forwardRef, useEffect, useState, useId } from "react";
 import type { ForwardedRef } from "react";
 import { Source } from "../tools/Source";
 import { makeStyles } from "../theme";
+import { useIntersectionObserver } from "../tools/useIntersectionObserver";
+import { useMergeRefs } from "powerhooks/useMergeRefs";
 
 export type GlVideoProps = {
     id?: string;
@@ -20,73 +22,98 @@ export type GlVideoProps = {
 };
 
 export const GlVideo = memo(
-    forwardRef((props: GlVideoProps, ref: ForwardedRef<HTMLVideoElement>) => {
-        const {
-            sources,
-            className,
-            hasShadow = true,
-            hasBorderRadius = true,
-            height,
-            id: id_props,
-            width,
-            autoPlay = true,
-            delayBeforeAutoPlay = 0,
-            loop,
-            muted,
-            controls,
-            onLoad,
-        } = props;
+    forwardRef(
+        (
+            props: GlVideoProps,
+            ref_forwarded: ForwardedRef<HTMLVideoElement>,
+        ) => {
+            const {
+                sources,
+                className,
+                hasShadow = true,
+                hasBorderRadius = true,
+                height,
+                id: id_props,
+                width,
+                autoPlay = true,
+                delayBeforeAutoPlay = 0,
+                loop,
+                muted,
+                controls,
+                onLoad,
+            } = props;
 
-        const { classes, cx } = useStyles({
-            hasShadow,
-            hasBorderRadius,
-        });
+            const { classes, cx } = useStyles({
+                hasShadow,
+                hasBorderRadius,
+            });
 
-        const [isDataLoaded, setIsDataLoaded] = useState(false);
+            const [isDataLoaded, setIsDataLoaded] = useState(false);
+            const [isIntersected, setIntersected] = useState(false);
 
-        const id = (function useClosure() {
-            const id = useId();
-            return id_props ?? id;
-        })();
+            const id = (function useClosure() {
+                const id = useId();
+                return id_props ?? id;
+            })();
 
-        useEffect(() => {
-            if (!isDataLoaded || !autoPlay) {
-                return;
-            }
+            const { ref: internalRef } = useIntersectionObserver(
+                {
+                    "callback": ({ observer, entry }) => {
+                        if (entry.isIntersecting) {
+                            observer.unobserve(entry.target);
+                            setIntersected(true);
+                        }
+                    },
+                    "threshold": 0.2,
+                },
+                [],
+            );
 
-            const timer = setTimeout(() => {
-                //@ts-expect-error: we know is will work
-                document.getElementById(id).play();
-            }, delayBeforeAutoPlay);
+            const ref = useMergeRefs([internalRef, ref_forwarded]);
 
-            return () => {
-                clearTimeout(timer);
-            };
-        }, [isDataLoaded]);
+            useEffect(() => {
+                if (delayBeforeAutoPlay === 0) {
+                    return;
+                }
 
-        return (
-            <video
-                ref={ref}
-                className={cx(classes.root, className)}
-                onLoadedData={() => {
-                    setIsDataLoaded(true);
-                    onLoad?.();
-                }}
-                loop={loop ?? true}
-                muted={muted ?? true}
-                autoPlay={false}
-                id={id}
-                width={width}
-                height={height}
-                playsInline={autoPlay ?? true}
-                controls={controls ?? false}
-            >
-                {sources.map(source => (
-                    <source {...source} />
-                ))}
-            </video>
-        );
-    }),
+                if (!isDataLoaded || !isIntersected) {
+                    return;
+                }
+
+                const timer = setTimeout(() => {
+                    //@ts-expect-error: we know is will work
+                    document.getElementById(id).play();
+                }, delayBeforeAutoPlay);
+
+                return () => {
+                    clearTimeout(timer);
+                };
+            }, [isDataLoaded]);
+
+            return (
+                <video
+                    ref={ref}
+                    className={cx(classes.root, className)}
+                    onLoadedData={() => {
+                        setIsDataLoaded(true);
+                        onLoad?.();
+                    }}
+                    loop={loop ?? true}
+                    muted={muted ?? true}
+                    autoPlay={delayBeforeAutoPlay === 0 && autoPlay}
+                    id={id}
+                    width={width}
+                    height={height}
+                    playsInline={true}
+                    controls={controls ?? false}
+                >
+                    {sources.map(source => (
+                        <source {...source} />
+                    ))}
+                </video>
+            );
+        },
+    ),
 );
 
 const useStyles = makeStyles<{ hasShadow: boolean; hasBorderRadius: boolean }>({
