632b688e3eafc52faa5089f6007538f740367f93
Joseph Garrone <joseph.garrone.gj@gmail.com>
2024-10-17 17:46:02 +0200

    No pointer on GlCard if not a link

--- src/GlCards/GlCard.tsx
+++ src/GlCards/GlCard.tsx
@@ -14,7 +14,7 @@ export type GlCardProps = {
 export const GlCard = memo((props: GlCardProps) => {
     const { children, link, className } = props;
 
-    const { classes, cx } = useStyles();
+    const { classes, cx } = useStyles({ "isLink": link !== undefined });
 
     return (
         <div
@@ -29,16 +29,20 @@ export const GlCard = memo((props: GlCardProps) => {
     );
 });
 
-const useStyles = tss.withName({ GlCard }).create(({ theme }) => ({
-    "root": {
-        "borderRadius": 16,
-        "transition": "box-shadow 200ms",
-        "margin": theme.spacing(2),
-        "boxShadow": theme.shadows[1],
-        "backgroundColor": theme.colors.useCases.surfaces.surface1,
-        ":hover": {
-            "boxShadow": theme.shadows[2],
-            "cursor": "pointer",
+const useStyles = tss
+    .withName({ GlCard })
+    .withParams<{ isLink: boolean }>()
+    .create(({ theme, isLink }) => ({
+        "root": {
+            "borderRadius": 16,
+            "transition": "box-shadow 200ms",
+            "margin": theme.spacing(2),
+            "boxShadow": theme.shadows[1],
+            "backgroundColor": theme.colors.useCases.surfaces.surface1,
+            "cursor": isLink ? "pointer" : undefined,
+            ":hover": {
+                "boxShadow": theme.shadows[2],
+                "cursor": "pointer",
+            },
         },
-    },
-}));
+    }));
